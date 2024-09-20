fb2c793ae61fef6def2f424f46488e454dcf26a1
Narendra Chitteti <122769756+narendrachitteti@users.noreply.github.com>
2024-09-20 11:25:34 +0530

    clerk

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ export default function App() {
   return (
     <header>
       <SignedOut>
-        <div style={{marginLeft:"250px", border:"1px solid grey", borderRadius:"5px"}}>
+        <center>
+        <div style={{border:"1px solid grey", borderRadius:"5px"}}>
         <SignInButton />
         into RaaP Portal
         </div>
+        </center>
       </SignedOut>
       <SignedIn>
         <div style={{marginLeft:"620px", marginTop:"-137px"}}>
